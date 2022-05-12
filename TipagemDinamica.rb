dfae46b0901583c86a8745ab109bef37379dcb6d
vngoliveira <vivi40195@gmail.com>
2022-05-11 22:58:15 -0300

    teste de tipagem Ruby

---
+++ TipagemDinamica.rb
@@ -0,0 +1,13 @@
+puts int = 7
+puts int.class
+puts ""
+puts f = 25.8
+puts f.class
+puts ""
+puts boo = false
+puts boo.class
+puts ""
+puts string = "Compass.Uol"
+puts string.class
+puts ""
+puts "Bye!"
