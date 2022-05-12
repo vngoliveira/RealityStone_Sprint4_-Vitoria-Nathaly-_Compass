41bab402e13ad27491e59173c9aa7ce4f5ddd2ac
vngoliveira <vivi40195@gmail.com>
2022-05-12 15:41:58 -0300

    exercício nome e idade em Ruby

---
+++ Desafio1Aula2.rb
@@ -0,0 +1,12 @@
+puts "Exercício nome e idade:"
+
+puts ""
+puts "Digite seu nome:"
+nome = gets.chomp
+puts ""
+puts "Digite sua idade:"
+idade = gets.chomp.to_i
+puts ""
+puts "Ok! Seu nome é #{nome} e sua idade é #{idade}!" 
+puts ""
+puts "Bye!"
