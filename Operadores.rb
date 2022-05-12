2ce9d3370526c17cfa498c04f02e0020fa795c8a
vngoliveira <vivi40195@gmail.com>
2022-05-11 23:19:12 -0300

    teste de operadores Ruby

---
+++ Operadores.rb
@@ -0,0 +1,44 @@
+puts "Os operadores matemáticos no Ruby são:"
+puts ""
+puts "Adição -> +"
+puts "Subtração -> -"
+puts "Multiplicação -> *"
+puts "Divisão -> /"
+puts "Módulo -> %"
+puts "Expoente -> **"
+puts ""
+puts "Algumas operações com eles:"
+puts ""
+puts "Adição:"
+puts 20+15 
+puts 35+87
+puts 2738+67
+puts 8+378
+puts ""
+puts "Subtração:"
+puts 84-83
+puts 783-32
+puts 10-7
+puts 39383-7382
+puts ""
+puts "Multiplicação:"
+puts 7*4
+puts 6*3
+puts 972*63
+puts 76*35
+puts ""
+puts "Divisão:"
+puts 7383/73
+puts 4/2
+puts 8372/84
+puts 63622/9
+puts ""
+puts "Módulo:"
+puts 24%2
+puts 7362%83
+puts ""
+puts "Expoente:"
+puts 76**3
+puts 8272**637
+puts ""
+puts "Bye!"
