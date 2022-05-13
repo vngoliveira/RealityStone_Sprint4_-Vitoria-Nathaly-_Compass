76dcac34e95fe2286265bb36b84410f125b4b646
vngoliveira <vivi40195@gmail.com>
2022-05-13 19:01:47 -0300

    exercício soma, sub, div e mult em Ruby

---
+++ Desafio2Aula2.rb
@@ -0,0 +1,12 @@
+puts "Exercício soma, subtração, multiplicação e divisão:"
+puts ""
+puts "Digite um número:"
+n1 = gets.chomp.to_i
+puts ""
+puts "Digite outro número:"
+n2 = gets.chomp.to_i
+puts ""
+puts "A soma entre #{n1} e #{n2} é igual a #{n1+n2}\n
+A subtração entre #{n1} e #{n2} é igual a #{n1-n2}\n
+A multiplicação entre #{n1} e #{n2} é igual a #{n1*n2}\n
+A divisão entre #{n1} e #{n2} é igual a #{n1/n2}\n"
